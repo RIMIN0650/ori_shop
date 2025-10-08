adc0e0d2cacdbdba651dccc66d41228f81ef5c5a
kwon_pc <chris0540@naver.com>
2025-10-08 22:29:14 +0900

    [권민석]  - 제품 등록 페이지

--- Ori_Shop/src/main/java/com/rimin/Ori_Shop/ItemController.java
+++ Ori_Shop/src/main/java/com/rimin/Ori_Shop/ItemController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -16,11 +18,27 @@ public class ItemController {
     @GetMapping("/item/list")
     String itemList(Model model){
         List<Item> itemList = itemRepository.findAll();
-        System.out.println(itemList);
+//        System.out.println(itemList);
         model.addAttribute("itemList", itemList);
 //        var a = new Item();
 //        System.out.println(a.toString());
         return "/list.html";
+    }
+
+    @GetMapping("/item/add")
+    String addItemPage() {
+        return "addItem.html";
+    }
+
+    @PostMapping("/item/add")
+    String addItem(@RequestParam(name = "itemName") String itemName,
+                   @RequestParam(name = "price") Integer price) {
+
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        itemRepository.save(item);
+        return "redirect:/item/list";
     }
 
 }

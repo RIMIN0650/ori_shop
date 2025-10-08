e501bf2b886f27284125a40e59445d779087863d
kwon_pc <chris0540@naver.com>
2025-10-08 14:35:21 +0900

    [권민석]  - 초기 세팅

---
+++ Ori_Shop/src/main/java/com/rimin/Ori_Shop/BasicController.java
@@ -0,0 +1,21 @@
+package com.rimin.Ori_Shop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BasicController {
+
+    @GetMapping("/")
+    @ResponseBody
+    String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/main")
+    String main() {
+        return "/index.html";
+    }
+
+}

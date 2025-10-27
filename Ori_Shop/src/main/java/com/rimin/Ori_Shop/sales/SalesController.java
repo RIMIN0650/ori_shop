0c935ab1272435480d0c7e28b0928075edfbff2d
kwon_pc <chris0540@naver.com>
2025-10-27 22:54:44 +0900

    [권민석]  - 장바구니 담기 기능 구현

--- Ori_Shop/src/main/java/com/rimin/Ori_Shop/sales/SalesController.java
+++ Ori_Shop/src/main/java/com/rimin/Ori_Shop/sales/SalesController.java
@@ -11,8 +11,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -21,20 +24,32 @@ public class SalesController {
     private final SalesRepository salesRepository;
     private final SalesService salesService;
 
+
+
+
     @PostMapping("/order")
-    String postorder(@RequestParam String itemName,
-                     @RequestParam Integer price,
-                     @RequestParam Integer count,
+    @ResponseBody
+    public Map<String, String> postorder(@RequestParam String itemName,
+                     @RequestParam Integer itemPrice,
+                     @RequestParam Integer itemCount,
                      Authentication auth) {
         Sales sales = new Sales();
         sales.setItemName(itemName);
-        sales.setPrice(price);
-        sales.setCount(count);
+        sales.setPrice(itemPrice);
+        sales.setCount(itemCount);
         CustomUser user = (CustomUser) auth.getPrincipal();
         sales.setMemberId(user.id);
         salesRepository.save(sales);
 
-        return "redirect:/main/home";
+        Map<String, String> resultMap = new HashMap<>();
+
+        if(sales != null){
+            resultMap.put("result", "success");
+        } else {
+            resultMap.put("result", "fail");
+        }
+
+        return resultMap;
     }
 
 
